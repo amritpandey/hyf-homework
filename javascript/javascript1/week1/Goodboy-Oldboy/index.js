b6ade28dd25ca198a1e0b1982fbc558994360112
amritpandey <amrit.pandey111@gmail.com>
2021-10-25 11:31:26 +0200

    remove commented part from index.js

--- javascript/javascript1/week1/Goodboy-Oldboy/index.js
+++ javascript/javascript1/week1/Goodboy-Oldboy/index.js
@@ -4,7 +4,7 @@ let shouldShowResultInDogYears = true; //boolean data type
 let humanYear = dogYearFuture - dogYearOfBirth;
 let  dogYear = humanYear * 7;
 
-//console.log(dogYear);
+
 
 if(shouldShowResultInDogYears){
     document.write(`Your dog will be ${dogYear} dog years old in ${dogYearFuture}`);
