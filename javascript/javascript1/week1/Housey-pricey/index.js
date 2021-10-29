fac15d4a6c29a078f040453037771f1855195d38
amritpandey <amrit.pandey111@gmail.com>
2021-10-29 11:22:53 +0200

    Modified Housey-Pricey index.js as per mentor Estefanes feedback

--- javascript/javascript1/week1/Housey-pricey/index.js
+++ javascript/javascript1/week1/Housey-pricey/index.js
@@ -1,26 +1,35 @@
-const firstFriend = "Peter";
-const secondFriend = "Julia";
-
+const firstPerson = "Peter";
 const houseCostFor100M2 = 25000000;
+const widthP = 8; heightP=10; breadthP = 10; gardenSizeP = 100;
+const volumeP = widthP*heightP*breadthP;
+
+const secondPerson = "Julia";
 const houseCostFor70M2 = 1000000;
+const widthJ = 5; heightJ=11; breadthJ = 8; gardenSizeJ = 70;
+const volumeJ = widthJ*breadthJ*heightJ;
 
-let volumeInMetersForPeter = 8*10*10;
-let gardenSizeInM2ForPeter = 100;
+let housePriceForPeter = volumeP * 2.5 * 1000 + gardenSizeP * 300;
+let housePriceForJulia = volumeJ * 2.5 * 1000 + gardenSizeJ * 300;
+//console.log("peter paid: " + housePriceForPeter+ "Julia paid: "+ housePriceForJulia );
 
-let volumeInMetersForJulia = 5*11*8;
-let gardenSizeInM2ForJulia = 70;
-
-let housePriceForPeter = volumeInMetersForPeter * 2.5 * 1000 + gardenSizeInM2ForPeter * 300;
-let housePriceForJulia = volumeInMetersForJulia * 2.5 * 1000 + gardenSizeInM2ForJulia * 300;
-console.log("peter paid: " + housePriceForPeter+ "Julia paid: "+ housePriceForJulia );
+let calculate = Math.abs(houseCostFor100M2 - housePriceForPeter );
+let calculateJ = Math.abs(houseCostFor70M2 - housePriceForJulia );
 
 if(houseCostFor100M2>housePriceForPeter){
-    console.log("Peter is paying too little for the house.");
+    console.log("Peter is paying "+ calculate +" less for the house.");
+}else if(houseCostFor100M2<housePriceForPeter){
+    console.log("Peter is paying " + calculate + " much for the house.");
 }else{
-    console.log("Peter is paying too much for the house.");
+    console.log("Peter is paying the right amount for the house.");
 }
+
 if(houseCostFor70M2>housePriceForJulia){
-    console.log("Julia is paying too little for the house.");
-}else{
-    console.log("Julia is paying too much for the house.");
+    console.log(`${secondPerson} is paying ${calculateJ} less for the house.`);
+}else if(houseCostFor70M2<housePriceForJulia){
+    console.log(`${secondPerson} is paying ${calculateJ} much for the house.`);
 }
+else{
+    console.log(`${secondPerson} is paying the right amount for the house.`);
+
+}
+
