a536cb3a5249f220b60021398879c3239d24aa40
amritpandey <amrit.pandey111@gmail.com>
2021-11-03 19:27:25 +0100

    Add event.js and index.html to event application

---
+++ javascript/javascript1/week2/Event-application/event.js
@@ -0,0 +1,13 @@
+ /*Event application*/
+
+  const weekdays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+        
+     function getEventWeekday(eventDay){
+        const d = new Date();
+        d.setDate(d.getDate() + eventDay);
+        let day = weekdays[d.getDay()];
+        
+        return day;
+    }
+        
+    console.log(getEventWeekday(3)); 
