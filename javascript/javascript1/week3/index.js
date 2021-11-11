9ced92e6b6325068394d5a594961cdecf5de334c
amritpandey <amrit.pandey111@gmail.com>
2021-11-11 01:02:30 +0100

    Added note taking appp

--- javascript/javascript1/week3/index.js
+++ javascript/javascript1/week3/index.js
@@ -92,7 +92,7 @@ const names = [
     for (let i = 0; i < seriesDurations.length; i++) {
     percentageValue = ((seriesDurations[i].days*24) + seriesDurations[i].hours)*100/lifespanInHours;
     
-    /* if(i===0){
+    if(i===0){
       console.log(`${seriesDurations[0].title} took ${percentageValue.toFixed(3)}% of my life` );
     }
     if(i===1){
@@ -100,16 +100,96 @@ const names = [
     }
     if(i===2){
       console.log(`${seriesDurations[2].title} took ${percentageValue.toFixed(3)}% of my life` );
-    } */
+    }
     totalPercentage +=percentageValue;
     }  
  
     console.log(`In total that is ${totalPercentage.toFixed(3)}% of my life`);
   }
- /*
-  
-   for (const item of seriesDurations) {
-    percentageValue = (item.days*24 + item.hours)*100/lifespanInHours;
-   }      
-    */
+ 
   console.log(calculateLifespanPercentage()); 
+
+  /**
+   * Note taking App
+   */
+
+  const notes = []; //empty array
+  
+  //function to save notes with two parameters
+  function saveNote(content, id){
+   let obj = {
+      content:content,
+      id:id
+    };
+    notes.push(obj);
+  }
+
+  saveNote("pick up groceries",1);
+  saveNote("Do laundry", 2);
+  saveNote("Cook food", 3);
+
+  console.log(notes);
+
+  // function to get a note
+  function getNote(id) {
+    for (const note of notes) {
+      if(note.id===id){
+        return note;
+      }
+    }
+  }
+  
+  const firstNote = getNote(1);
+  console.log(firstNote); // {content: 'Pick up groceries', id: 1}
+
+  // function to log out notes
+  function logOutNotesFormatted() {
+    // your code here
+     for (const note of notes) {
+      console.log(`The note with id: ${note.id}, has the following note text: ${note.content}`);
+    } 
+ /*  for (let index = 0; index < notes.length; index++) {
+    console.log(`The note with id: ${notes[index].id}, has the following note text: ${notes[index].content}`);
+
+  } */
+  }
+  
+  console.log(logOutNotesFormatted());
+
+  function updateContentOfNote(id, content){
+    for (const note of notes) {
+      if(note.id===id){
+        note.content="hello";
+      }
+    }
+  }
+  const firstUpdate = updateContentOfNote(3, "Dont cook food, i will buy");
+  console.log(firstUpdate);
+
+
+  /**
+   * Smart phone usage app
+   */
+
+  const activities=[];
+  function addActivity(date, activity, duration){
+    let obj1 = {
+      date:date,
+      activity:activity,
+      duration:duration
+    };
+    activities.push(obj1);
+  }
+
+  //addActivity("23/7-18", "Youtube", 30);
+  console.log(activities);
+
+  function showStatus(activities){
+    if(activities===""){
+      console.log("Add some activities before calling showStatus");
+    }else{
+      console.log("You have added 3 activities. They amount to 78 min. of usage");
+    }
+  }
+
+  showStatus(activities);
