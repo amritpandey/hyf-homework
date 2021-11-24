7bb632149bdd25dfbb764402e88af8f1bca710c4
amritpandey <amrit.pandey111@gmail.com>
2021-11-24 20:02:32 +0100

    Added list style in js and removed from css file

--- javascript/javascript2/week1/hyf-bay/main.js
+++ javascript/javascript2/week1/hyf-bay/main.js
@@ -11,6 +11,8 @@ function renderProducts(products) {
     
     const list = document.createElement('li');
     list.innerHTML= `<h2>${product.name}</h2> Rating: ${product.rating}</br> Price: ${product.price}`;
+    list.style.margin="20px";
+    list.style.borderBottom="solid 2px";
     allProducts1.appendChild(list);
   }
 }
