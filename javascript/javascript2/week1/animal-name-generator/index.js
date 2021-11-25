7bfafd3cfa02988fd41a41ac487590f020a54232
amritpandey <amrit.pandey111@gmail.com>
2021-11-25 15:05:02 +0100

    Added spirit animal name generator

---
+++ javascript/javascript2/week1/animal-name-generator/index.js
@@ -0,0 +1,45 @@
+//Spirit animal part
+
+//const userName = prompt("Enter your name");
+
+const spiritAnimalNames = [
+    'The crying butterfly',
+    'The full moon wolf',
+    'The dangerous tiger',
+    'The gigantic giraffe',
+    'The big old elephant',
+];
+
+//getting random numbers from spiritAnimalNames
+let randomNumber = Math.floor(Math.random() * spiritAnimalNames.length);
+
+const myNewButton = document.querySelector('#myButton');
+
+function clickFunction() {
+    myNewButton.addEventListener('click', function () {
+        if (document.querySelector('#click').checked) {
+            getUsernameWithSpirit();
+        }
+    });
+}
+
+function hoverFunction() {
+    myNewButton.addEventListener('mouseover', function () {
+        if (document.querySelector('#hover').checked) {
+            getUsernameWithSpirit();
+        }
+    });
+}
+
+function getUsernameWithSpirit() {
+    const val = document.querySelector('#user-name').value;
+    if (val) {
+        const output = `${val} - ${spiritAnimalNames[randomNumber]}`;
+        document.querySelector('.displaySpirit').innerHTML = output;
+    } else {
+        alert("Input field can't be empty!!!");
+    }
+}
+
+clickFunction();
+hoverFunction();
