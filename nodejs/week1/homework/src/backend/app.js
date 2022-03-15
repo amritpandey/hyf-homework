8428b5d7c64fa6b6ca91745eb065081a87d3afee
amritpandey <amrit.pandey111@gmail.com>
2022-03-15 04:14:37 +0100

    Updated according to mentors comment, new function getMapAndFilter added.

--- nodejs/week1/homework/src/backend/app.js
+++ nodejs/week1/homework/src/backend/app.js
@@ -6,93 +6,66 @@ const meals = require('./data/meals.json');
 const reservations = require('./data/reservations.json');
 const reviews = require('./data/reviews');
 
+app.get('/', sendGreeting);
+app.get('/meals', listMeals);
+app.get('/reservations', listReservations);
+app.get('/cheap-meals', cheapMealsList);
+app.get('/large-meals', largeMealsList);
+app.get('/meal', listMeal);
+app.get('/reservation', listReservation);
 
-app.get("/", sendGreeting);
-app.get("/meals", listMeals);
-app.get("/reservations", listReservations);
-app.get("/cheap-meals", cheapMealsList);
-app.get("/large-meals", largeMealsList);
-app.get("/meal", listMeal);
-app.get("/reservation", listReservation);
-
-app.get('/', async (request, response) => {
+async function sendGreeting(request, response) {
     response.send('asd');
-});
-
-async function sendGreeting(request, response)  {
-  response.send('asd');
-};
+}
 
 async function listMeals(request, response) {
-    const allTheMeals = meals.map((meal) => {
-        const reviewOfEachMeal = reviews.filter(
-            (review) => review.mealId === meal.id,
-        );
-        return {
-            ...meal,
-            reviews: reviewOfEachMeal,
-        };
-    });
-    response.json(allTheMeals);
+    response.json(getMapAndFilter(meals));
+}
+
+function getMapAndFilter(param) {
+  const mealWithReview = param.map(meal => {
+    const reviewOfEachMeal = reviews.filter(review => review.mealId === meal.id)
+    return {
+      ...meal,
+      reviews: reviewOfEachMeal
+    }
+  })
+  return mealWithReview
 };
 
-async function cheapMealsList(request, response){
-    const allCheapMeals = meals
+async function cheapMealsList(request, response) {
+     const allCheapMeals = meals
         .filter((meal) => meal.price < 80)
-        .map((cheapMeal) => {
-            const reviewOfEachCheapMeal = reviews.filter(
-                (review) => review.mealId === cheapMeal.id,
-            );
-            return {
-                ...cheapMeal,
-                reviews: reviewOfEachCheapMeal,
-            };
-        });
-    response.json(allCheapMeals);
-};
+    response.json(getMapAndFilter(allCheapMeals));
+}
 
-async function largeMealsList(request, response){
+async function largeMealsList(request, response) {
     const largeMeals = meals
         .filter((meal) => meal.maxNumberOfGuests > 20)
-        .map((largeMeal) => {
-            const reviewOfEachLargeMeal = reviews.filter(
-                (review) => review.mealId === largeMeal.id,
-            );
-            return {
-                ...largeMeal,
-                reviews: reviewOfEachLargeMeal,
-            };
-        });
-    response.json(largeMeals);
-};
+        
+    response.json(getMapAndFilter(largeMeals));
+}
 
-async function listMeal(request, response){
-        const reviewOfRandMeal = reviews
-        .filter((review) => review.mealId === randomValue(meals))
-        .map((randomMeal) => {
-            const reviewOfEachRandomMeal = reviews.filter(
-                (review) => review.mealId === randomMeal.id,
-            );
-            return {
-                ...randomMeal,
-                reviews: reviewOfEachRandomMeal,
-            };
-        });
-    response.json(reviewOfRandMeal);
-};
+async function listMeal(request, response) {
+    let randomMeal = meals[randomValue(meals)]; 
+    const reviewOfRandMeal = meals
+        .filter((meal) => meal.id === randomMeal.id);
+       
+    response.json(getMapAndFilter(reviewOfRandMeal));
+}
 
-async function listReservations(request, response){
+async function listReservations(request, response) {
     response.json(reservations);
-};
+}
 
 function randomValue(upperLimit) {
-  const random = Math.round(Math.random() * upperLimit.length + 1);
-return random;
+    const random = Math.round(Math.random() * upperLimit.length);
+    return random;
 }
 
 async function listReservation(request, response) {
-    const allRes = reservations.filter((randRes) => randRes.id === randomValue(reservations));
-    response.json(allRes);
-};
+    const randomIndex = reservations[randomValue(reservations)];
+    response.json(randomIndex);
+}
 
 module.exports = app;
