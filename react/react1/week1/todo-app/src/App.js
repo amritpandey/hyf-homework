4a599aa855031dd75f987441394786b8268fbb9a
amritpandey <amrit.pandey111@gmail.com>
2022-04-07 23:26:57 +0200

    Unecessary header removed from app.js

--- react/react1/week1/todo-app/src/App.js
+++ react/react1/week1/todo-app/src/App.js
@@ -4,9 +4,7 @@ import { TodoList } from './components/TodoList';
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
                  <TodoList/>
-            </header>
         </div>
     );
 }
